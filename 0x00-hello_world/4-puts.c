2e9f0a2f8e061eec0981cedc5e3675e8694f35a3
Mustafa-Yaqout <sasa.yaqout.197@gmail.com>
2023-08-11 14:27:04 -0700

    ${var}

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- * 
+ *
  * Description: A C program that prints with put function
-
- * Return: Always 0 (Success)
+ *
+ * Return: Alawys 0 (Success)
 */
 
 int main(void)
