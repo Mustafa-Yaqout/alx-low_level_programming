dd46e24f61eb45e310a9fcea335bc168b7952970
Mustafa-Yaqout <sasa.yaqout.197@gmail.com>
2023-08-21 14:57:02 -0700

    daws

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include "holberton.h"
 
 /**
  * main - check the code
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * reset_to_98 - Updates the value it points to 98
-  * @n: pointer to n
-  *
-  * Return: void
-  */
+ * reset_to_98 - a function that takes a pointer to an int as parameter and
+ * updates the value it points to to 98
+ * @n: the integer adresse
+ *
+ * Return: 1 or 0
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
